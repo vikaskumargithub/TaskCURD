b4ee3321c04216f1c61ff6ec8ef7ac25652a6bf6
vikas kumar <108663989+vikaskumargithub@users.noreply.github.com>
2024-05-13 22:39:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,34 +31,8 @@ app.post('/getPersons', (req, res) => {
     });
 });
 
-app.get("/getPersons",(req,res)=>{
-    if (data.length<=0) {
-        return res.status(404).json({
-            status:"fail",
-            message:"wasn't able to get data",
-            data:data
-        })
-    }
-    res.status(200).json({
-        status:"success",
-        message:"successfully fetched data",
-        data:data
-    })
-})
 
 
-
-app.put("/getPersons/:id",(req,res)=>{
-    let singlePerson = data.find(doc=>doc.id===+req.params.id)
-    let newBody = req.body
-    if (!singlePerson) {
-        return res.status(404).json({
-            status:"fail",
-            message:"there is no document with this id"
-        })
-    }
-    res.status(201).json({...singlePerson,newBody   })
-})
 
 app.delete("/getPersons/:id", (req, res) => {
     const id = +req.params.id;
@@ -80,26 +54,10 @@ app.delete("/getPersons/:id", (req, res) => {
     });
 });
 
-app.get("/createData/:id", (req, res) => {
-    let person = data.find(doc => doc.name === req.params.name);
-    if (!person) {
-        return res.status(404).json({
-            status: "fail",
-            message: "There is no person with this name"
-        });
-    }
+
 
    
-    let newDataEntry = { id: data.length + 1, personId: person.id, ...req.body };
-    data.push(newDataEntry);
 
-    res.status(201).json({
-        status: "success",
-        data: {
-            newDataEntry
-        }
-    });
-});
 
 
 
